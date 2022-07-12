2a10e71524eeaabb64fc283259d41d5badfa8531
Oscar Menacho <58416396+Omr30@users.noreply.github.com>
2022-07-12 09:33:49 -0500

    Expect jest use

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,14 +3,11 @@
 
 describe('Example Component', () => {
   test( 'Debe de ser mayor a 10', () => {
-    let value = 9
+    let value =9
 
     value = value + 2
 
-    if( value > 10 ) {
-      // TODO: todo bien!
-    } else {
-      throw `${ value } no es mayor a 10`
-    }
+    expect( value  ).toBeGreaterThan( 10 )
+
   })
 })
