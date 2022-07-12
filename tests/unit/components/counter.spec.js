196125343cb6e519a1a8b43ab88b09b94979e18b
Oscar Menacho <58416396+Omr30@users.noreply.github.com>
2022-07-12 10:01:45 -0500

    Verificar valor en una etiqueta HTML

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -3,11 +3,24 @@ import Counter from '@/components/Counter'
 
 
 describe('Counter Component', () => { 
-    test('debe de hacer match con el snapshot', () => { 
+    // test('debe de hacer match con el snapshot', () => { 
+
+    //     const wrapper = shallowMount( Counter )
+
+    //     expect( wrapper.html() ).toMatchSnapshot()
+
+    // })
+
+    test('h2 debe tener el valor por defecto "Counter"', () => { 
 
         const wrapper = shallowMount( Counter )
 
-        expect( wrapper.html() ).toMatchSnapshot()
+        expect( wrapper.find('h2').exists() ).toBeTruthy()
+
+        const h2Value = wrapper.find('h2').text()
+        
+        expect( h2Value ).toBe('Counter')
 
     })
+
 })
